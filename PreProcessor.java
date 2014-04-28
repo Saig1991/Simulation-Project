9f2e2ed6ba365c8f090b4be28060c8d100813069
sai <sai96@hotmail.com>
2014-04-28 15:57:58 +1000

    Changed PreProcessor to become more streamlined in terms of the simulator

--- PreProcessor.java
+++ PreProcessor.java
@@ -1,50 +1,33 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.io.IOException;
-import java.lang.String;
-import java.lang.Double;
 import org.apache.commons.math3.distribution.ExponentialDistribution;
 
 // The class that will compute the pre-processing of a multi-server system.
 public class PreProcessor {
 
-   /*
-    * Read in the inter arrival times from the file specified in the input.
-    */
-   public ArrayList<Double> getInterArrivalTimes(String fileName) {
-      BufferedReader br  = null;
-      String currentLine;
-      ArrayList<Double> interArrivalTimes = new ArrayList<Double>();
+    private static int numSubTasks;
+    private static int currentTime;
 
+    public PreProcessor(int n){
+        numSubTasks = n;
+        currentTime = 0;
+    }
 
-      try {
-            br = new BufferedReader(new FileReader(fileName));
-            while((currentLine = br.readLine()) != null) {
-                double time = Double.parseDouble(currentLine);
-                interArrivalTimes.add(time);
-            }
-
-      } catch (IOException e){
-        e.printStackTrace();
-      } finally {
-        try {
-            if (br != null)br.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-      }
-
-      return interArrivalTimes;
-   }
 
    // Time it takes the pre-processor to process a request into n sub-requests.
-   public double  processRequest (int n) {
-        double ExpDistRate = 10/n;
+   // n has to be less than the number of servers m.
+   public void  processRequest (Request newRequest, double arrivalTime) {
+
+        double ExpDistRate = 10/numSubTasks;
         ExponentialDistribution ed = new ExponentialDistribution(ExpDistRate);
         double processTime = ed.sample();
 
-        return processTime;
+        if(currentTime < arrivalTime) currentTime = arrivalTime;
+        currentTime += processTime;
+
+        for(int i = 0; i < numSubTasks; i++){
+            SubRequest newSub = new SubRequest(currentTime);
+            newRequest.add(newSub);
+        }
+
    }
 
 
