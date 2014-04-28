f4ae93742e2c7d265ababf0eca57de097373e17c
sai <sai96@hotmail.com>
2014-04-28 15:59:09 +1000

    Created an Arrival Times class that reads the inter arrival times of the requests

---
+++ ArrivalTimes.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.lang.String;
+import java.lang.Double;
+
+// The class that will compute the pre-processing of a multi-server system.
+public class PreProcessor {
+
+   /*
+    * Read in the inter arrival times from the file specified in the input.
+    */
+   public ArrayList<Double> getInterArrivalTimes(String fileName) {
+      BufferedReader br  = null;
+      String currentLine;
+      ArrayList<Double> interArrivalTimes = new ArrayList<Double>();
+
+
+      try {
+            br = new BufferedReader(new FileReader(fileName));
+            while((currentLine = br.readLine()) != null) {
+                double time = Double.parseDouble(currentLine);
+                interArrivalTimes.add(time);
+            }
+
+      } catch (IOException e){
+        e.printStackTrace();
+      } finally {
+        try {
+            if (br != null)br.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+      }
+
+      return interArrivalTimes;
+   }
+}
