3c6e0b1f8dd6ec9f487e338023269ca5771d0378
sai <sai96@hotmail.com>
2014-05-05 13:53:05 +1000

    Added the facilities to process a request and send it to the multiple servers

--- MultiServer.java
+++ MultiServer.java
@@ -4,13 +4,42 @@ import java.util.ArrayList;
 public class MultiServer {
     
     private ArrayList<Server> multiServers;
+    private int n;
 
-    public MultiServer(int m){
+    public MultiServer(int m, int n){
+        this.n = n;
+        multiServers = new ArrayList<Server>();
         for(int i = 0; i < m; i++){
             Server newServer = new Server();
             multiServers.add(newServer);
         }
     }
+
+    private Server findIdleServer(SubRequest sr){
+       Server retVal = multiServers.get(0);
+       for(int i = 0; i < multiServers.size(); i++){
+            Server temp = multiServers.get(i);
+            if(temp.getCurrTime() < sr.getProcessTime()){
+                retVal = temp;    
+                break;
+            }
+       }
+
+       return retVal;
+
+    }
+
+    public void processRequest(Request req){
+       int numSubRequests = req.getNumSubRequests(); 
+       for(int i = 0; i < numSubRequests; i++){
+           SubRequest temp = req.getSubRequest(i);
+           Server getIdleServer = findIdleServer(temp);
+           getIdleServer.processSubRequest(n,temp);
+       }
+
+            
+    }
+
 
 
 
