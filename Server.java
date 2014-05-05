63be1f922dce4722389eb829608f5ca7c92b34d2
sai <sai96@hotmail.com>
2014-05-05 12:48:29 +1000

    Updated the server class with the pareto distribution information given by the lecturer

--- Server.java
+++ Server.java
@@ -1,9 +1,12 @@
 import java.lang.Math;
 //import org.apache.commons.math3.distribution.ParetoDistribution;
+import org.apache.commons.math3.distribution.UniformRealDistribution;
+import org.apache.commons.math3.random.JDKRandomGenerator;
 
 public class Server {
     private static final double shape = 2.08;
     private static double prevSubReqTime;
+    private static final int seed = 3;
 
     public void processSubRequest(int n, SubRequest newReq){
         double ts = ParetoCalc(n);
@@ -19,16 +22,33 @@ public class Server {
 
     }
 
+    // Generate a random number using the pareto distribution.
+    // ts = tm/(n^1.7 * logk(1-u)) by using the inverse method on a pareto distribution with CDF.
+    // let U = 1-u which is a uniformly distributed random number.
     private double ParetoCalc(int n){
         double tm = ( 20 * (shape - 1) )/ (shape);
         double scale = tm/(Math.pow(n,1.7));
         double ts = 0;
 
+        // Setting a predefined see for simulation purposes.
+        JDKRandomGenerator rng = new JDKRandomGenerator();
+        rng.setSeed(seed);
+        UniformRealDistribution urd = new UniformRealDistribution(rng,0,1);
+        double urdRand = 0;
 
-        ParetoDistribution pd = new ParetoDistribution(scale, shape);
-        ts = pd.sample();
+        // Get a uniformly distributed random number (0,1).
+        while(urdRand == 0){
+            urdRand = urd.sample();
+        }
+
+        ts = scale/logOfBase(urdRand,shape);
 
         return ts;
+    }
+
+
+    public double logOfBase(double base, double num) {
+            return Math.log(num) / Math.log(base);
     }
 
 }
