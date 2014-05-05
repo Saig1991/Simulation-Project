f400e810b0a582a86ac578fff0693941b45035c4
sai <sai96@hotmail.com>
2014-05-05 12:58:47 +1000

    Added seeding for the random number generators for reproducibility.

--- GenerateRandNo.java
+++ GenerateRandNo.java
@@ -4,9 +4,8 @@ import java.util.Random;
  
 import org.apache.commons.math3.distribution.GammaDistribution;
 import org.apache.commons.math3.distribution.TriangularDistribution;
-import org.apache.commons.math3.distribution.UniformRealDistribution;
-import org.apache.commons.math3.random.AbstractRandomGenerator;
 import org.apache.commons.math3.random.RandomGenerator;
+import org.apache.commons.math3.random.JDKRandomGenerator;
  
  
 public class GenerateRandNo {
@@ -17,22 +16,28 @@ public class GenerateRandNo {
         private double a = 0.1;
         private double b = 1.5;
         private double c = 0.8;
+
+        private static final int seed = 3;
        
        
         //private double Fc = 0.5;
        
-        public RandomGenerator rng;
        
         public double gammaDist () {
-                GammaDistribution gD = new GammaDistribution (g_scale, shape);
+                JDKRandomGenerator rng = new JDKRandomGenerator();
+                rng.setSeed(seed);
+
+                GammaDistribution gD = new GammaDistribution (rng,g_scale, shape,GammaDistribution.DEFAULT_INVERSE_ABSOLUTE_ACCURACY);
                 return gD.sample();
         }
        
         public double triangularDist () throws InstantiationException, IllegalAccessException {
                 double U = 0.0;
                 double X = 0.0;
+
                 TriangularDistribution tD = new TriangularDistribution (a, c, b);
                 Random rnd = new Random();
+                rnd.setSeed(seed);
                 U = rnd.nextDouble();
                 //System.out.println("U = " + U);
                 double Fc = (c-a)/(b-a);
