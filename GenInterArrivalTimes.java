e91d60caeb4cb3ee77caae43c552ba90274a6414
sai <sai96@hotmail.com>
2014-05-14 13:54:32 +1000

    Changed so that the output file name is based of command line inputs

--- GenInterArrivalTimes.java
+++ GenInterArrivalTimes.java
@@ -14,26 +14,30 @@ public class GenInterArrivalTimes {
        
         public static void main(String[] args) throws InstantiationException, IllegalAccessException, IOException {
                
-                genInterArrivalSeq();
+			   
+				int numArrivals = Integer.parseInt(args[0]);
+				int arrivalName = Integer.parseInt(args[1]);
+                genInterArrivalSeq(numArrivals, arrivalName);
                 //System.out.println("Gamma value = " + gRN.gammaDist());
                 //System.out.println("Triangular Dis value = " + gRN.triangularDist());
         }
        
-        public static void genInterArrivalSeq () throws InstantiationException, IllegalAccessException, IOException {
+        public static void genInterArrivalSeq (int numArrivals, int arrivalName) throws InstantiationException, IllegalAccessException, IOException {
                
-                File file = new File ("arrivalTimes.txt");
+				String fileName = "arrivalTimes_" + arrivalName + ".txt";
+                File file = new File (fileName);
                 FileWriter fw = new FileWriter (file.getAbsoluteFile());
                 BufferedWriter bw = new BufferedWriter (fw);
                 int i = 1;
-                while (i <= 10) {
+                while (i <= numArrivals) {
                         double a1K = gRN.gammaDist();
                         double a2K = gRN.triangularDist();
                         double aK = (a1K + a2K) * Math.abs(a1K-a2K);
                         String toWrite = Double.toString(aK);
                         bw.write(toWrite);
                         bw.write("\n");
-                        //System.out.println("a1k + a2k = " + gRN.gammaDist()+gRN.triangularDist());
-                        System.out.println("a"+i+" = " + aK);
+//                        System.out.println("a1k + a2k = " + gRN.gammaDist()+gRN.triangularDist());
+  //                      System.out.println("a"+i+" = " + aK);
                         //arrival.add(aK);
                         i++;
                 }
